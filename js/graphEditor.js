4bf5f9cc4c361d086341be98d69dd159738d0323
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-17 18:31:07 +0530

    event listner mousedown

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -5,18 +5,31 @@ class GraphEditor {
  
        this.ctx = this.canvas.getContext("2d");
 
+       this.selected = null;
+       this.hovered = null;
+
        this.#addEventListeners();
     }
 
     #addEventListeners() {
         this.canvas.addEventListener("mousedown", (evt) => {
             const mouse = new Point(evt.offsetX, evt.offsetY);
+            this.hovered = getNearestPoint(mouse, this.graph.points, 10);
+            if (this.hovered) {
+                this.selected = this.hovered;
+                return;
+
+            }
             this.graph.addPoint(mouse);
+            this.selected = mouse;
         });
     }
 
     display() {
         this.graph.draw(this.ctx);
+        if (this.selected) {
+            this.selected.draw(this.ctx, { outline: true  });
+        }
     }
 }
 
