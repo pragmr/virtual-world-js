1fa3c5a4448f974cb16dd4e36a9465e538a95ff8
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-17 17:34:59 +0530

    Graph Editor

---
+++ js/graphEditor.js
@@ -0,0 +1,13 @@
+class GraphEditor {
+    constructor(canvas, graph) {
+       this.canvas = canvas;
+       this.graph = graph;
+ 
+       this.ctx = this.canvas.getContext("2d");
+    }
+
+    display() {
+        this.graph.draw(this.ctx);
+    }
+}
+
