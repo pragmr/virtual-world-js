8c58c7a2bb9dc0520d16547a7f1e37f103f42708
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-17 18:50:10 +0530

    remove point mouse right click

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -13,20 +13,40 @@ class GraphEditor {
 
     #addEventListeners() {
         this.canvas.addEventListener("mousedown", (evt) => {
-            const mouse = new Point(evt.offsetX, evt.offsetY);
-            this.hovered = getNearestPoint(mouse, this.graph.points, 10);
-            if (this.hovered) {
-                this.selected = this.hovered;
-                return;
+            if (evt.button == 2) { // right click
+                if (this.hovered) {
+                    this.graph.removePoint(this.hovered);
+                }
 
             }
-            this.graph.addPoint(mouse);
-            this.selected = mouse;
+            if (evt.button == 0) {  //left click
+
+           
+                const mouse = new Point(evt.offsetX, evt.offsetY);
+                
+                if (this.hovered) {
+                    this.selected = this.hovered;
+                    return;
+
+                }
+                this.graph.addPoint(mouse);
+                this.selected = mouse;
+            }
+       
+        });
+
+        this.canvas.addEventListener("mousemove", (evt) => {
+            const mouse = new Point(evt.offsetX, evt.offsetY);
+            this.hovered = getNearestPoint(mouse, this.graph.points, 10);
+           
         });
     }
 
     display() {
         this.graph.draw(this.ctx);
+        if (this.hovered) {
+            this.hovered.draw(this.ctx, { fill: true  });
+        }
         if (this.selected) {
             this.selected.draw(this.ctx, { outline: true  });
         }
