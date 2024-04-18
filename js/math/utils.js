286429929d882a3154c15969c4d69437227ac085
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-18 19:43:12 +0530

    feature#116 (viewport.js) any size of graph

--- js/math/utils.js
+++ js/math/utils.js
@@ -24,3 +24,8 @@ function getNearestPoint(loc, points, threshold = Number.MAX_SAFE_INTEGER) {
    return new Point(p1.x - p2.x, p1.y - p2.y);
 
  }
+
+ function scale(p, scaler) {
+   return new Point(p.x * scaler, p.y * scaler);
+
+ }
