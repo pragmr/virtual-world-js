b6677e6f7e5dd3729818ff09fa584484709bdfa4
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-19 20:55:49 +0530

    feature #119 generating roads completed

--- js/world.js
+++ js/world.js
@@ -24,9 +24,13 @@ class World {
     
  
     draw(ctx) {
-       for (const env of this.envelopes) {
-          env.draw(ctx);
-       }
+        for (const env of this.envelopes) {
+            env.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15 });
+         }
+
+         for (const seg of this.graph.segments) {
+            seg.draw(ctx, { color: "white", width: 4, dash: [10, 10] });
+         }
        for (const seg of this.roadBorders) {
         seg.draw(ctx, { color: "white", width: 4 });
      }
