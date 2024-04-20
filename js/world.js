d5b85ae0374284ba7b547d70bca584db6dac2e9c
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-20 23:45:39 +0530

    feature #121 points are genertaed where trees need to be added

--- js/world.js
+++ js/world.js
@@ -21,7 +21,8 @@ class World {
        this.envelopes = [];
        this.roadBorders = [];
        this.buildings = [];
- 
+       this.trees = [];
+
        this.generate();
     }
  
@@ -35,8 +36,32 @@ class World {
 
        this.roadBorders = Polygon.union(this.envelopes.map((e) => e.poly));
        this.buildings = this.#generateBuildings();
-      
+       this.trees = this.#generateTrees();
     }
+
+   #generateTrees(count = 10) {
+      const points = [
+         ...this.roadBorders.map((s) => [s.p1, s.p2]).flat(),
+         ...this.buildings.map((b) => b.points).flat()
+
+      ];
+
+      const left = Math.min(...points.map((p) => p.x));
+      const right = Math.max(...points.map((p) => p.x));
+      const top = Math.min(...points.map((p) => p.y));
+      const bottom = Math.max(...points.map((p) => p.y));
+
+
+      const trees = [];
+      while (trees.length < count) {
+         const p = new Point(
+            lerp(left, right,Math.random()),
+            lerp(bottom, top,Math.random())
+         );
+         trees.push(p);
+      }
+      return trees;
+   }
 
     #generateBuildings() {
       const tmpEnvelopes = [];
@@ -112,6 +137,10 @@ class World {
        for (const seg of this.roadBorders) {
         seg.draw(ctx, { color: "white", width: 4 });
      }
+
+     for (const tree of this.trees) {
+      tree.draw(ctx);
+   }
      for (const bld of this.buildings) {
       bld.draw(ctx);
    }
