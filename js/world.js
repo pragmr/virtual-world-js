56cb8306ee8c02c86e442143e77d106d22a867f7
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-22 17:44:22 +0530

    feature #124 segment at an offset, colour green

--- js/world.js
+++ js/world.js
@@ -71,7 +71,7 @@ class World {
          // check if tree too close to other trees
          if (keep) {
             for (const tree of trees) {
-               if (distance(tree, p) < this.treeSize) {
+               if (distance(tree.center, p) < this.treeSize) {
                   keep = false;
                   break;
                }
@@ -91,7 +91,7 @@ class World {
          }
 
          if (keep) {
-            trees.push(p);
+            trees.push(new Tree(p, this.treeSize));
             tryCount = 0;
          }
          tryCount++;
@@ -175,7 +175,7 @@ class World {
       }
 
       for (const tree of this.trees) {
-         tree.draw(ctx, { size: this.treeSize, color: "rgba(0,0,0,0.5)" });
+         tree.draw(ctx);
       }
       for (const bld of this.buildings) {
          bld.draw(ctx);
