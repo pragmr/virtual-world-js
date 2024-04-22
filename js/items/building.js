2e73a680c557429cd1dfbbdc6d24d8e652f67084
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-22 19:12:38 +0530

    feature #124 building( ceiling of the building) top points

--- js/items/building.js
+++ js/items/building.js
@@ -5,6 +5,14 @@ class Building {
     }
  
     draw(ctx, viewPoint) {
+
+        const topPoints = this.base.points.map((p) =>
+          add(p, scale(subtract(p, viewPoint), this.heightCoef))
+    );
+
+    const ceiling = new Polygon(topPoints);
        this.base.draw(ctx, { fill: "white", stroke: "#AAA"});
+      ceiling.draw(ctx, { fill: "white", stroke: "#AAA"});
+
     }
  }
