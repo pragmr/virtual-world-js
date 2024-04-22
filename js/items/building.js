d1d3b1eb4ec26add93c63525adbbb2dad6eb0238
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-22 19:20:41 +0530

    feature #124 sides of the buildings

--- js/items/building.js
+++ js/items/building.js
@@ -11,7 +11,20 @@ class Building {
     );
 
     const ceiling = new Polygon(topPoints);
+
+    const sides = [];
+    for (let i = 0; i < this.base.points.length; i++) {
+       const nextI = (i + 1) % this.base.points.length;
+       const poly = new Polygon([
+          this.base.points[i], this.base.points[nextI],
+          topPoints[nextI], topPoints[i]
+       ]);
+       sides.push(poly);
+    }
        this.base.draw(ctx, { fill: "white", stroke: "#AAA"});
+       for (const side of sides) {
+        side.draw(ctx, { fill: "white", stroke: "#AAA" });
+     }
       ceiling.draw(ctx, { fill: "white", stroke: "#AAA"});
 
     }
