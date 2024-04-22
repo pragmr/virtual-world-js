885dc6eea0350a136d465370708ed4d6a9614e2d
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-22 18:10:37 +0530

    cylinders with rgb colours

--- js/items/tree.js
+++ js/items/tree.js
@@ -1,7 +1,8 @@
 class Tree {
-    constructor(center, size) {
+    constructor(center, size, heightCoef = 0.3) {
        this.center = center;
        this.size = size; // size of the base
+       this.heightCoef = heightCoef;
        
     }
  
@@ -9,9 +10,19 @@ class Tree {
  
     draw(ctx, viewPoint) {
       const diff = subtract(this.center, viewPoint);
-      this.center.draw(ctx, { size: this.size, color: "lightgreen" });
+      const top = add(this.center, scale(diff, this.heightCoef));
+      
+      
+      const levelCount = 7;
+      for (let level = 0; level < levelCount; level++) {
+         const t = level / (levelCount - 1);
+         const point = lerp2D(this.center, top, t);
+         const color = "rgb(30," + lerp(50, 200, t) + ",70)";
+         const size = lerp(this.size, 40, t);
+         point.draw(ctx, { size: this.size, color });
+      }
+   
 
-      const top = add(this.center, diff);
-      new Segment(this.center, top).draw(ctx);
-    }
- }
+   }
+}
+ 
