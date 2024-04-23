7c2544a296752aae55fced706ba02a3bcd3062f4
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-23 17:26:04 +0530

    feature #125 crossing perpendicular lie is added

--- js/markings/crossing.js
+++ js/markings/crossing.js
@@ -15,7 +15,13 @@ class Crossing  {
     }
  
     draw(ctx) {
-        this.poly.draw(ctx);
+      const perp = perpendicular(this.directionVector);
+      const line = new Segment(
+         add(this.center, scale(perp, this.width / 2)),
+         add(this.center, scale(perp, -this.width / 2))
+      );
+      line.draw(ctx);
+
       
 
     }
