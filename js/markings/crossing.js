a93f6319abe56420741f2b5f7e8f9f96effe864a
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-23 17:33:48 +0530

    feature #125 crossings are added with debugger

--- js/markings/crossing.js
+++ js/markings/crossing.js
@@ -11,7 +11,7 @@ class Crossing  {
 
       );
       this.poly = new Envelope(this.support, width, 0).poly;
-      this.border = this.poly.segments[2];
+      this.borders = [this.poly.segments[0], this.poly.segments[2]];
     }
  
     draw(ctx) {
@@ -20,8 +20,15 @@ class Crossing  {
          add(this.center, scale(perp, this.width / 2)),
          add(this.center, scale(perp, -this.width / 2))
       );
-      line.draw(ctx);
+      line.draw(ctx, {
+        width: this.height,
+        color: "white",
+        dash: [11, 11]
+     });
 
+     for(const b of this.borders){
+      b.draw(ctx);
+     }
       
 
     }
