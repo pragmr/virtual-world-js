ebba5f63f23a309d731f99ab0d8c40ef1a604681
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-23 17:21:18 +0530

    feature #125 added crossing button with crossing.js and crossingEditor.js files

---
+++ js/markings/crossing.js
@@ -0,0 +1,22 @@
+class Crossing  {
+    constructor(center, directionVector, width, height) {
+       this.center = center;
+       this.directionVector = directionVector;
+       this.width = width;
+       this.height = height;
+ 
+      this.support = new Segment(
+        translate(center, angle(directionVector), height / 2),
+        translate(center, angle(directionVector), -height / 2)
+
+      );
+      this.poly = new Envelope(this.support, width, 0).poly;
+      this.border = this.poly.segments[2];
+    }
+ 
+    draw(ctx) {
+        this.poly.draw(ctx);
+      
+
+    }
+ }
