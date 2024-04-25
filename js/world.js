f1bb2fe7190f5afc310d045c5cc75bbbae907ff1
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-25 21:03:23 +0530

    feature #128 trees are added

--- js/world.js
+++ js/world.js
@@ -36,6 +36,8 @@ class World {
       world.envelopes = info.envelopes.map((e) => Envelope.load(e));
       world.roadBorders = info.roadBorders.map((b) => new Segment(b.p1, b.p2));
       world.buildings = info.buildings.map((e) => Building.load(e));
+      world.trees = info.trees.map((t) => new Tree(t.center, info.treeSize));
+
 
 
       return world;
